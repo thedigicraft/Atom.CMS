4f86b944b068190e03dfe3f8a9b0738264b1d2bf
Alan Quandt <alan@rtistics.com>
2013-12-26 15:21:07 -0500

    commenting and some cleanup

--- config/setup.php
+++ config/setup.php
@@ -1,23 +1,20 @@
 <?php
 ## Setup Document
 
-// host(or location of the database), username, password, database name
-
+// host(or location of the database), username, password, database name:
 $dbc = mysqli_connect('localhost', 'yourusername', 'yourpassword', 'yourdatabasename') OR die ('Could not connect to the datebase because: ' . mysqli_connect_error() );
 
+// Function Files:
+include('functions/sandbox.php'); // Sandbox Functions.
+include('functions/template.php'); // Template Functions.
 
-include('functions/sandbox.php');
-include('functions/template.php');
-
+// IF a page name is not given:
 if ($_GET['page'] == '') {
-	$pg = 'home';
+	$pg = 'home'; // Default to the Home Page
 } else {
-	$pg = $_GET['page'];
+	$pg = $_GET['page']; // Use the page in the URL
 }
 
-$page_title = get_page_title($dbc, $pg);
-
-
-
+$page_title = get_page_title($dbc, $pg); // Get the page title.
 
 ?>
--- index.php
+++ index.php
@@ -8,7 +8,7 @@ include('config/setup.php');
 <html>
 <head>
 <meta charset="utf-8">
-<title><?php echo $page_title; ?> - ATOM.CMS</title>
+<title><?php echo $page_title; // Title of the Page ?> - ATOM.CMS</title>
 
 <link rel="stylesheet" type="text/css" href="css/styles.css">
 
@@ -18,9 +18,9 @@ include('config/setup.php');
 
     <div class="wrap_overall">
     
-        <div class="header"><?php head(); ?></div>
+        <div class="header"><?php head(); ?></div><!-- END header -->
     
-        <div class="nav_main"><?php nav_main(); ?></div>
+        <div class="nav_main"><?php nav_main(); ?></div><!-- END nav_main -->
     
         <div class="content">
             <?php 
@@ -29,13 +29,13 @@ include('config/setup.php');
 				else { get_page($dbc, $pg); }
 				
 			?>    
-        </div>
+        </div><!-- END content -->
     
     	<div class="clear"></div>
     
-        <div class="footer"><?php footer(); ?></div>
+        <div class="footer"><?php footer(); ?></div><!-- END footer -->
         
-    </div>
+    </div><!-- END wrap_overall -->
 
 </body>
 </html>
