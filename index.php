c21e25d4a7c6253cec7ec503589be534bd4d05c6
The Digital Craft <alan@rtistics.com>
2013-12-26 15:26:07 -0500

    Update index.php

--- index.php
+++ index.php
@@ -25,10 +25,10 @@ include('config/setup.php');
         <div class="content">
             <?php 
 			
-				if ($pg == 'blog') { get_blog($dbc); } 
-				else { get_page($dbc, $pg); }
+		if ($pg == 'blog') { get_blog($dbc); } 
+		else { get_page($dbc, $pg); }
 				
-			?>    
+	    ?>    
         </div><!-- END content -->
     
     	<div class="clear"></div>
