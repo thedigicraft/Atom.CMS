5a71d2ea5d8d182eda17574485e6457182f5235b
Alan Quandt <acc@rtistics.com>
2014-02-05 00:30:25 -0500

    Version 2.0 Initial Commit

---
+++ template/footer.php
@@ -0,0 +1,7 @@
+<footer id="footer">
+	
+	<div class="container">
+		<p>This is my footer.</p>
+	</div>
+	
+</footer><!-- END footer -->
---
+++ template/navigation.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-default" role="navigation">
+	
+	<?php if($debug == 1) { ?>
+		<button id="btn-debug" class="btn btn-default"><i class="fa fa-bug"></i></button>
+	<?php } ?>
+	
+	<div class="container">	
+		
+		<ul class="nav navbar-nav">
+			
+			<?php nav_main($dbc, $pageid); ?>
+			
+			<li><a href="#">FAQ</a></li>
+			<li><a href="#">Contact</a></li>
+		</ul>
+		
+	</div>
+	
+</nav><!-- END nav -->
