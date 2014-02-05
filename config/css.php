bedc0f7a2c6bf71bdc2197e4b67e631389f6c9de
Alan Quandt <acc@rtistics.com>
2014-02-05 00:35:22 -0500

    Version 2.0 Initial Commit

---
+++ config/css.php
@@ -0,0 +1,66 @@
+<?php
+// CSS:
+
+
+
+
+
+?>
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap-theme.min.css">
+
+<!-- jQuery CSS -->
+<link rel="stylesheet" href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui-custom.css">
+
+<!-- FontAwesome -->
+<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+
+<style>
+	
+	html,
+	body {
+	  height: 100%;
+	  /* The html and body elements cannot have any padding or margin. */
+	}
+	
+	/* Wrapper for page content to push down footer */
+	#wrap {
+	  min-height: 100%;
+	  height: auto;
+	  /* Negative indent footer by its height */
+	  margin: 0 auto -60px;
+	  /* Pad bottom by footer height */
+	  padding: 0 0 60px;
+	}
+	
+	/* Set the fixed height of the footer here */
+	#footer {
+	  height: 60px;
+	  background-color: #f5f5f5;
+	}
+	
+	#btn-debug {
+		position: absolute;
+	}	
+	
+	#console-debug {
+		position: absolute;
+		top: 50px;
+		left:0px;
+		width:30%;
+		height:700px;
+		overflow-y:scroll;
+		background-color: #FFFFFF;
+		box-shadow: 2px 2px 5px #CCCCCC;
+	}
+	#console-debug pre {
+		
+		
+	}
+	
+	
+</style>
---
+++ config/setup.php
@@ -0,0 +1,36 @@
+<?php
+// Setup File:
+
+# Database Connection Here...
+$dbc = mysqli_connect('localhost', 'dev', 'thepassword1', 'atomcms') OR die('Could not connect because: '.mysqli_connect_error());
+
+# Constants:
+DEFINE('D_TEMPLATE', 'template');
+
+# Functions:
+include('functions/data.php');
+include('functions/template.php');
+
+# Site Setup:
+$debug = data_setting_value($dbc, 'debug-status');
+
+$site_title = 'AtomCMS 2.0';
+
+if(isset($_GET['page'])) {
+	
+	$pageid = $_GET['page']; // Set $pageid to equal the value given in the URL
+	
+} else {
+	
+	$pageid = 1; // Set $pageid equal to 1 or the Home Page.
+	
+}
+
+# Page Setup:
+$page = data_page($dbc, $pageid);
+
+
+
+
+
+?>
