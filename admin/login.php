c4eca4c8b7102b402ecfdfc0639c212e255ea578
Alan Quandt <acc@rtistics.com>
2014-02-07 02:37:31 -0500

    Video 25
    The state of the project at Video 25

--- admin/login.php
+++ admin/login.php
@@ -29,29 +29,41 @@ include('../config/connection.php');
 	
 			<h1>Login</h1>
 			
-			<form role="form">
+			<div class="row">
 				
-			  <div class="form-group">
-			    <label for="exampleInputEmail1">Email address</label>
-			    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
-			  </div>
-			  
-			  <div class="form-group">
-			    <label for="exampleInputPassword1">Password</label>
-			    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-			  </div>
-			  
-			  <!--
-			  <div class="checkbox">
-			    <label>
-			      <input type="checkbox"> Check me out
-			    </label>
-			  </div>
-			  -->
-			  
-			  <button type="submit" class="btn btn-default">Submit</button>
-			  
-			</form>		
+				
+				<div class="col-md-4 col-md-offset-4">
+					
+					<form role="form">
+						
+					  <div class="form-group">
+					    <label for="exampleInputEmail1">Email address</label>
+					    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+					  </div>
+					  
+					  <div class="form-group">
+					    <label for="exampleInputPassword1">Password</label>
+					    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+					  </div>
+					  
+					  <!--
+					  <div class="checkbox">
+					    <label>
+					      <input type="checkbox"> Check me out
+					    </label>
+					  </div>
+					  -->
+					  
+					  <button type="submit" class="btn btn-default">Submit</button>
+					  
+					</form>					
+					
+				</div>
+				
+								
+			</div>
+			
+		
 			
 		</div>
 
